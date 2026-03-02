364d05af839a932603cf7fb39c28b2e9972263b4
Sumit Kumar Kushwaha <sumitkmr62@gmail.com>
2026-03-02 13:16:54 +0000

    Commit 2

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,3 +1,16 @@
---select * from `dbt-tutorial.jaffle_shop.customers`;
---select * from `dbt-tutorial.jaffle_shop.orders`;
-select * from `dbt-tutorial.stripe.payment`
+/*
+    Welcome to my first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized="table") }}
+
+with source_data as (
+    select 1 as id
+    union all
+    select null as id
+)
+select * from source_data
